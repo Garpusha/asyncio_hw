0205412f6d8dd83edcef6d8b16f1f0501c7f506e
Garpusha <alexander.sbitnev@yandex.ru>
2023-09-17 23:56:02 +0300

    Initial commit

---
+++ models.py
@@ -0,0 +1,39 @@
+import os
+
+from sqlalchemy import JSON, Column, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+PG_USER = os.getenv("PG_USER", "user")
+PG_PASSWORD = os.getenv("PG_PASSWORD", "password")
+PG_DB = os.getenv("PG_DB", "sw")
+PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
+PG_PORT = os.getenv("PG_PORT", "5431")
+
+PG_DSN = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+
+engine = create_async_engine(PG_DSN)
+Session = sessionmaker(class_=AsyncSession, expire_on_commit=False, bind=engine)
+
+Base = declarative_base()
+
+
+class SwapiPeople(Base):
+    __tablename__ = "swapi_people"
+
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer)
+    birth_year = Column(String)
+    eye_color = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    films = Column(String)
+    gender = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    name = Column(String)
+    species = Column(String)
+    starships = Column(String)
+    vehicles = Column(String)
